4a2f72b10516e80f294a702e7f48130c1c975c30
manuja-xo62 <manuja.work61@gmail.com>
2026-06-08 15:55:07 +0530

    created the basic log in, sign up, log out functionalities auth skel for later firebase integration

--- app/components/AuthCOntext.tsx
+++ app/components/AuthCOntext.tsx
@@ -10,3 +10,68 @@ interface AuthContextProps {
     logout: () => void;
     register: (email: string, password: string, role: Role) => void;
 }
+
+//firebase integration
+
+export const AuthContext = createContext<AuthContextProps | undefined>(undefined);
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [role, setRole] = useState(null);
+    const isAuthenticated = !!user;
+
+    useEffect(() => {
+        const savedUser = localStorage.getItem("auth-user");
+        const savedRole = localStorage.getItem("auth-role") as Role | null;
+        if (savedUser && savedRole) {
+            setUser(savedUser);
+            setRole(savedRole);
+        }
+    }, []);
+
+    const login = (email: string, password: string) => {
+        //firebase integration
+        if (email && password) {
+            const token = crypto.randomUUID();
+            localStorage.setItem("auth-token", token);
+            localStorage.setItem("auth-role", email);
+            setUser(email);
+        } else {
+            alert("Please provide email & Password")
+        }
+    };
+
+
+
+    const register = (email: string, password: string, role: Role) => {
+        //firebase integration
+        const token = crypto.randomUUID();
+        localStorage.setItem("auth-token", token);
+        localStorage.setItem("auth-role", role);
+        localStorage.setItem("auth-user", email);
+        setUser(email);
+        setRole(role);
+    };
+
+    const logout = () => {
+        //firebase integration
+        localStorage.clear();
+        setUser(null);
+        setRole(null);
+    };
+
+    return (
+        <AuthContext.Provider
+            value={{ user, role, isAuthenticated, login, logout, register }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if (!context) {
+        throw new Error("useAuth must be used within AuthProvider");
+    }
+    return context;
+};
