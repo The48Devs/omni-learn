73b39806dbb76a0c53c1b97ed1531129307ba0e8
manuja-xo62 <manuja.work61@gmail.com>
2026-06-08 23:58:11 +0530

    wrapped student portal with the auth gaurd

--- app/StudentPortal/dashboard/page.tsx
+++ app/StudentPortal/dashboard/page.tsx
@@ -1,12 +1,15 @@
 import { useAuth } from "@/app/components/AuthCOntext";
+import RequireAuth from "@/app/components/RequireAuth";
 
 export default function StudentDashboard() {
     const { user } = useAuth();
     return (
-        <main className="p-8 bg-[var(--bg-primary)] text-[var(--text-main)]">
-            <h1 className="text-3xl font-bold mb-4">
-                Welcome, {user ?? "Student"} (Student Dashboard)
-            </h1>
-        </main>
+        <RequireAuth role="student">
+            <main className="p-8 bg-[var(--bg-primary)] text-[var(--text-main)]">
+                <h1 className="text-3xl font-bold mb-4">
+                    Welcome, {user ?? "Student"} (Student Dashboard)
+                </h1>
+            </main>
+        </RequireAuth>
     );
 }
