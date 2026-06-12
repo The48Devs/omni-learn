61ebeefaefc385223d723db293aab74b4b202f28
manuja-xo62 <manuja.work61@gmail.com>
2026-06-12 19:37:10 +0530

    created the modules * content blocks states

--- app/TutorStudio/mycourses/newcourse/page.tsx
+++ app/TutorStudio/mycourses/newcourse/page.tsx
@@ -20,11 +20,11 @@ interface ContentBlock {
     title: string;
     duration?: string;
     sandboxComponents?: string[];
-    quizQuesntions?: QuizQuestions[];
+    quizQuestions?: quizQuestions[];
 }
 
-interface QuizQuestions {
-    d: string;
+interface quizQuestions {
+    id: string;
     question: string;
     options: { text: string; isCorrect: boolean }[];
 }
@@ -40,5 +40,53 @@ export default function CourseCreatorStudio() {
     const [courseTitle, setCourseTitle] = useState("Introduction to Current Electricity");
     const [courseDescription, setCourseDescription] = useState("Learn the fundamentals of electrical currents, conductors, insulators, and circuit design.");
     const [courseSubject, setCourseSubject] = useState("Physics");
+
+    //Modules * content block states
+    const [modules, setModules] = useState<Module[]>([
+        {
+            id: "mod-1",
+            index: 1,
+            title: "Introduction to Current Electricity",
+            duration: "30 minutes",
+            blocks: [
+                {
+                    id: "block-1",
+                    type: "video",
+                    title: "Video Lesson: Introduction to Electronics",
+                    duration: "12 mins"
+                },
+                {
+                    id: "block-2",
+                    type: "sandbox",
+                    title: "Sandbox: Understanding Current Electricity",
+                    sandboxComponents: ["Battery", "LED"]
+                },
+                {
+                    id: "block-3",
+                    type: "quiz",
+                    title: "Current Electricity Quiz",
+                    quizQuestions: [
+                        {
+                            id: "q-1",
+                            question: "Which particle transmits electricity?",
+                            options: [
+                                { text: "Electrons", isCorrect: true },
+                                { text: "Neutrons", isCorrect: false },
+                                { text: "Protons", isCorrect: false },
+                                { text: "None", isCorrect: false }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            id: "mod-2",
+            index: 2,
+            title: "Electrical Conductors & Insulators",
+            duration: "45 minutes",
+            blocks: []
+        }
+    ]);
 
 }
